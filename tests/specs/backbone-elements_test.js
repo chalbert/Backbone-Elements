5dee463b960f4ba9ba19f97446a6b748e17c7dcf
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-03-31 08:47:44 +1300

    Added test for 'refresh elements'

--- tests/specs/backbone-elements_test.js
+++ tests/specs/backbone-elements_test.js
@@ -1,13 +1,14 @@
 define([
   'jquery',
+  'underscore',
   'backbone',
   'backbone-elements'
-], function($, Backbone){
+], function($, _, Backbone){
 
-  describe("$get", function () {
+  describe("Backbone-Elements", function () {
 
     beforeEach(function(){
-      $('body').append('<div id="container"><div id="content"></div></div>');
+      $('body').append('<div id="container"><div id="content"><a></a><div class="box"></div></div></div>');
       var view = Backbone.View.extend({
         el: '#container',
         elements: {
@@ -22,12 +23,35 @@ define([
       this.view.remove();
     });
 
-    it("Returns the jQuery object from an element name.", function () {
-      expect(this.view.$get('content')).toBe($('#content'));
+    describe("$get()", function () {
+
+      it("Returns the jQuery object from an element name.", function () {
+        expect(this.view.$get('content')).toBe($('#container').find('#content'));
+        expect(this.view.$get('link')).toBe($('#container').find('a'));
+        expect(this.view.$get('box')).toBe($('#container').find('.box'));
+      });
+
+    });
+
+    describe("refreshElements()", function () {
+
+      it("Should create a cached version for each element", function () {
+
+        _.extend(this.view.elements, {
+          link: 'a',
+          box: '.box'
+        });
+
+        this.view.refreshElements();
+
+        expect(this.view.$content).toBe(this.view.$get('content'));
+        expect(this.view.$link).toBe(this.view.$get('link'));
+        expect(this.view.$box).toBe(this.view.$get('box'));
+      });
+
     });
 
   });
-
 
 
 });
